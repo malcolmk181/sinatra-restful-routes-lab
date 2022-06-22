bcd2675571150d2e095c7f1b261d6d05ab4e7dd2
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-21 21:53:01 -0400

    commit tree d103f50b4cf10f7fa9628dad483eaf1c4cf39a36

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,41 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+
+    erb :index
+  end
+
+  post '/recipes' do
+    r = Recipe.create(params)
+    redirect "/recipes/#{r.id}"
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+
+    erb :show
+  end
+
+  delete '/recipes/:id' do
+    Recipe.find(params[:id]).destroy
+    redirect "/recipes"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    Recipe.find(params[:id]).update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{params[:id]}"
+  end
+
 
 end
